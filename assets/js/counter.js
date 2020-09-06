f989663e2d7693f04f3c482dd485d62674bfc577
Alejandro Bringas <bringasalejandro@gmail.com>
2020-09-06 11:18:43 +0200

    Add counter animation

---
+++ assets/js/counter.js
@@ -0,0 +1,12 @@
+// Creates the animation effect of counting from 0 to any specified number
+$('.counter').each(function () {
+  $(this).prop('Counter',0).animate({
+      Counter: $(this).text()
+  }, {
+      duration: 2000,
+      easing: 'swing',
+      step: function (now) {
+          $(this).text(Math.ceil(now));
+      }
+  });
+});
